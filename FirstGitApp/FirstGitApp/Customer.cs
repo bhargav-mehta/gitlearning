61da1e1df3d36fbad153c83607513b1f72f18051
Bhargav <bhargav.mehta@wolterskluwer.com>
2017-12-11 12:48:36 +0000

    Made Customer class public

--- FirstGitApp/FirstGitApp/Customer.cs
+++ FirstGitApp/FirstGitApp/Customer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FirstGitApp
 {
-    class Customer
+    public class Customer
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
